203e2b05f866f9c3b53120700719a87d06d4d672
Mark Mickan <mark.mickan@netspot.com.au>
2014-10-22 07:57:40 +1030

    Tidy up fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,7 @@
 . /opt/boxen/env.fish
 
-#set -g -x PATH /usr/local/bin $PATH
-#set -g -x fish_greeting ''
-#set -g -x EDITOR /usr/bin/vim
-
-# rbenv setup
-#set -g -x PATH /opt/boxen/rbenv/bin $PATH
-#. (rbenv init -|psub)
-
-# put ./bin in the path for use with bundler's binstubs ("bundle install --binstubs")
-#set -g -x PATH ./bin $PATH
+set -g -x fish_greeting ''
+set -g -x EDITOR /usr/bin/vim
 
 # add a "tree" command like the one included in Ubuntu
 alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
