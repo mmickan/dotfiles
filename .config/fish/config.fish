f6d603ba01c848cb774f5cd4541616c61ef42395
Mark Mickan <mark.mickan@netspot.com.au>
2014-10-22 08:08:41 +1030

    Add CDPATH environment variable for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 . /opt/boxen/env.fish
 
-set -g -x fish_greeting ''
+set --erase fish_greeting
 set -g -x EDITOR /usr/bin/vim
+set -g -x CDPATH . ~
 
 # add a "tree" command like the one included in Ubuntu
 alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
